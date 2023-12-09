2a678638b129067a6a6daa1295f7494c3a6d8d8b
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-12-09 20:09:05 +0900

    Change data to list and Create else case

--- scrapper.py
+++ scrapper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from is_today import is_today
+from post import Post
 from send_message import send_message
 
 def scrapper():
@@ -25,25 +26,20 @@ def scrapper():
     
     html = driver.page_source
     soup = bs(html, "html.parser")
-
-    elements = soup.select('tbody > tr > td > a')
-
-    for index, element in enumerate(elements, 1):
-        print("{} 번째 공지사항 제목: {}, link: https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(index, element.text, element.attrs['href']))
     
     dateList = soup.select('.list-right')
+    elements = soup.select('tbody > tr > td > a')
 
-    for element in dateList:
-        if is_today(element.text.strip()):
-            print(element.text.strip())
+    count = sum(1 for element in dateList if is_today(element.text.strip()))
+    posts = []
+    
+    for index, element in enumerate(elements[:count], 1):
+        print("https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(element.attrs["href"]))
+        driver.get("https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(element.attrs["href"]))
+        driver.implicitly_wait(1)
+        title = driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/h2').text
+        content = driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/div[2]/p/img').get_attribute('src')
+        print("{} 번째 공지사항 제목: {}, link: https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(index, element.text, element.attrs['href']))
+        posts.append(Post(title = title, content = content))
 
-    driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div[2]/div[1]/table/tbody/tr[1]/td[2]/a').click()
-    driver.implicitly_wait(2)
-
-    title = driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/h2').text
-    content = driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/div[2]/p/img').get_attribute('src')
-
-    print(title)
-    print(content)
-
-    send_message(title, content)
+    send_message(posts = posts)
--- send_message.py
+++ send_message.py
@@ -1,29 +1,66 @@
 from venv import logger
 from dotenv import load_dotenv
+from post import Post
 import requests
 import os
 
-def send_message(title: str, content: str):
-    try:
-        load_dotenv()
-        slack_url = os.getenv('PERSONAL_SLACK')
-        header = {'Content-type': 'application/json'}
-        icon_emoji = ":rice:"
-        username = "WhatMenu"
-        attachments = [{
-            "color": "good",
-            "author_name": title,
-            "image_url": content,
-        }]
+def send_message(posts: [Post]):
+    index = 0
 
-        data = {"username": username, "attachments": attachments, "icon_emoji": icon_emoji}
-        print(data)
+    load_dotenv()
+    slack_url = os.getenv('PERSONAL_SLACK')
+    header = {'Content-type': 'application/json'}
+    icon_emoji = ":rice:"
+    username = "WhatMenu"
+    if(len(posts) > 0): 
+        try:
+            attachments = [{
+                "color": "good",
+                "author_name": posts[index].title,
+                "image_url": posts[index].content,
+                "actions": [
+                    {
+                        "name": "before",
+                        "text": "이전으로",
+                        "type": "button",
+                        "value": "before"
+                    },
+                    {
+                        "name": "next",
+                        "text": "다음으로",
+                        "type": "button",
+                        "value": "next"
+                    },
+                ]
+            }]
 
-        # 메세지 전송
-        return requests.post(slack_url, headers=header, json=data)
+            data = {"username": username, "attachments": attachments, "icon_emoji": icon_emoji}
+            print(data)
+
+            # 메세지 전송
+            return requests.post(slack_url, headers=header, json=data)
         
-    except Exception as e:
-        logger.error("Slack Message 전송에 실패했습니다.")
-        logger.error("에러 내용 : " + e.__str__)
+        except Exception as e:
+            logger.error("Slack Message 전송에 실패했습니다.")
+            logger.error("에러 내용 : " + e.__str__)
 
-        exit(0)
+            exit(0)
+
+    else:
+        try:
+            attachments = [{
+                "color": "good",
+                "text": "오늘은 공지가 없네요"                
+            }]
+
+            data = {"username": username, "attachments": attachments, "icon_emoji": icon_emoji}
+            print(data)
+
+            # 메세지 전송
+            return requests.post(slack_url, headers=header, json=data)
+        
+        except Exception as e:
+            logger.error("Slack Message 전송에 실패했습니다.")
+            logger.error("에러 내용 : " + e.__str__)
+
+            exit(0)
