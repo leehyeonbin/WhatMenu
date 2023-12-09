cc1ecc3e4b1017290cbf1aee80a85ffc729cdae9
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-12-09 18:43:20 +0900

    Remove print

--- is_today.py
+++ is_today.py
@@ -2,5 +2,4 @@ import datetime as dt
 
 def is_today(date: str):
     today = dt.datetime.now().strftime("%y-%m-%d")
-    print("{} == {} = {}".format(date, today, date == today))
     return date == today
