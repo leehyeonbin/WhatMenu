d8b54fea74eb48ea4ab4fe1eaff73f1956b4874a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-12-10 19:24:03 +0900

    Change the scrapper to send just menu

--- scrapper.py
+++ scrapper.py
@@ -33,13 +33,16 @@ def scrapper():
     count = sum(1 for element in dateList if is_today(element.text.strip()))
     posts = []
     
-    for index, element in enumerate(elements[:count], 1):
+    # for index, element in enumerate(elements[:count], 1):
+    for index, element in enumerate(elements, 1):
         print("https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(element.attrs["href"]))
         driver.get("https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(element.attrs["href"]))
         driver.implicitly_wait(1)
         title = driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/h2').text
         content = driver.find_element('xpath', '/html/body/div/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/div[2]/p/img').get_attribute('src')
         print("{} 번째 공지사항 제목: {}, link: https://hubkitchen.startup-plus.kr/cms_for_bcb/process/notice/{}".format(index, element.text, element.attrs['href']))
-        posts.append(Post(title = title, content = content))
+        if "[키친인큐베이터] 메뉴공지" in element.text:
+            posts.append(Post(title = title, content = content))
+            break
 
     send_message(posts = posts)
